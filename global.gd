e1a1b0712a26eaa20a2706f9a2d8db2566b6818e
Cold-iu <colescharly@yahoo.com>
2023-09-14 20:29:13 -0400

    Revert "Completes the mouse and keyboard exercise""
    This reverts commit 3daeaeb0e1c9f69068952adc1e1f265e00b48a53.

--- global.gd
+++ global.gd
@@ -1,18 +1,13 @@
 extends Node
 
-var Score = 0
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	randomize()
-	print(Score)
-	
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_pressed("Exit"):
 		get_tree().quit()
-	
-	
+	pass
